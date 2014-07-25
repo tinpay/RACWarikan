a2922813b3071cab4ff09d43ca673eba7df4be0b
cw-fukui <fukui@chatwork.co.jp>
2014-07-25 16:12:43 +0900

    RAC

---
+++ QuickWarikanNormal/QuickWarikan/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QuickWarikan
+//
+//  Created by shohei on 2014/07/07.
+//  Copyright (c) 2014年 Tinpay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TPYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TPYAppDelegate class]));
+    }
+}
